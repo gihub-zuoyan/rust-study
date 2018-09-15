4a66ea9919bd32719017b8b00a022cb1a2d87e64
liuguilin <liuguilin@qiyi.com>
2018-09-15 18:59:49 +0800

    编写测试用例

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    const MAX : u32 = 100;
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
 }
